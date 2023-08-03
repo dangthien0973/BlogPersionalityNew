7e24b5214cc1dc7a53baf893df0697239ef9c643
ducle <ducle@atmaneuler.com>
2023-08-03 11:00:28 +0700

    up

--- src/app/layout/context-menu/context-menu.component.ts
+++ src/app/layout/context-menu/context-menu.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit ,AfterViewInit} from '@angular/core';
+import * as $ from 'jquery';
 @Component({
   selector: 'app-context-menu',
   templateUrl: './context-menu.component.html',
   styleUrls: ['./context-menu.component.css']
 })
-export class ContextMenuComponent implements OnInit {
+export class ContextMenuComponent implements  OnInit {
 
   constructor() { }
 
   ngOnInit() {
+    $('.navbar-toggler').on('click', function () {
+      $('.navbar-collapse').toggleClass('collapse');
+    });
   }
-
+    click() {
+      $('.navbar-toggler').on('click', function () {
+        $('.navbar-collapse').toggleClass('collapse');
+      });
+  }
 }
