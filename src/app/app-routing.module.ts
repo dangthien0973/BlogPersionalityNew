fddee41e76b8634dd55507c20879d89d8df1b5d8
DangThien <18130224@st.hcmuaf.edu.vn>
2023-11-05 14:38:12 +0700

    up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,13 +10,14 @@ import { ContentRouting } from './main/blog/content-blog/content-rounting.module
 import { ListBlogComponent } from './main/blog/content-blog/list-blog/list-blog.component';
 import { PostBlogComponent } from './main/internal/post-blog/post-blog.component';
 const routes: Routes = [
-    { path: 'login-component', component: LoginComponent  },
+    { path: 'login-component', component: LoginComponent},
     {path:'menu', component: ContextMenuComponent},
     {path:'blog ', component: BlogComponent},
     {path:'about-me', component: AboutMeComponent},
-    { path: '',component: BlogComponent },
+    { path: 'home',component: BlogComponent },
     { path: 'listblog',component: ListBlogComponent },
-    { path: 'postblog',component: PostBlogComponent }
+    { path: 'postblog',component: PostBlogComponent },
+    { path: '',   redirectTo: '/home', pathMatch: 'full' }, // 
   ];
 
 @NgModule({
